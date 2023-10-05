f9e4e9b925b9cbd26181d8028ef6f27b056ee34e
itzrossyo <rossespley@gmail.com>
2023-10-05 22:38:33 +0100

    completed day2 your life in weeks challnge

---
+++ day2_your_life_in_weeks.py
@@ -0,0 +1,7 @@
+age = (input("please enter you age"))
+Total_years = 90
+total_weeks = 4696
+years_left = Total_years - int(age)
+weeks_left = int(years_left * 52)
+
+print(f"you have {weeks_left} weeks left.")
